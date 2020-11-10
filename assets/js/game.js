7ce23b1dccd2d4f3a17521fb5f92b3820dbd139f
Jacob Hilton <jakehilton@jacobs-mbp.lan>
2020-11-10 13:16:09 -0700

    console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  var playerName = window.prompt("what is your robot's name?");
+// What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message")
+// this will do math and log 20
+console.log(10+10);
+// What is this?
+console.log("Our robot's name is " + playerName)
+
+ // fight ();
